b7deaa9822e754889a417d901803a4c58cc31ce0
周琰杰 (Zhou Yanjie) <zhouyanjie@wanyeetech.com>
2020-02-15 23:02:20 +0800

    MIPS: JZ4780: Introduce SMP support.
    Forward port smp support from kernel 3.18.3 of CI20_linux
    to upstream kernel 5.6.
    
    Tested-by: H. Nikolaus Schaller <hns@goldelico.com>
    Tested-by: Paul Boddie <paul@boddie.org.uk>
    Signed-off-by: 周琰杰 (Zhou Yanjie) <zhouyanjie@wanyeetech.com>
    Reviewed-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- arch/mips/jz4740/setup.c
+++ arch/mips/jz4740/setup.c
@@ -19,6 +19,7 @@
 
 #include <asm/bootinfo.h>
 #include <asm/fw/fw.h>
+#include <asm/mach-jz4740/smp.h>
 #include <asm/prom.h>
 #include <asm/reboot.h>
 #include <asm/time.h>
@@ -100,6 +101,9 @@ void __init plat_time_init(void)
 void __init prom_init(void)
 {
 	fw_init_cmdline();
+
+	if (IS_ENABLED(CONFIG_SMP))
+		jz4780_smp_init();
 }
 
 void __init prom_free_prom_memory(void)
