77d4cf3526e362b4a5707cd73917a605495deda3
周琰杰 (Zhou Yanjie) <zhouyanjie@wanyeetech.com>
2020-12-05 21:10:43 +0800

    MIPS: mm: Add Ingenic XBurst SoCs specific cache driver.
    Although Ingenic's SoCs with XBurst®1 or XBurst®2 CPU cores can use
    the c-r4k.c cache driver, but it does not make full use of the unique
    features of Ingenic's CPU cores. To apply these feature in c-r4k.c,
    largescale modifications are needed, which may adversely affect other
    processors that use this driver. Therefore, a new c-ingenic.c driver
    optimized for the specific features of the Ingenic CPU was created.
    
    According to tests, the SoCs using the XBurst®1 or XBurst®2 CPU cores
    can obtain varying degrees of performance improvement after using this
    driver. Details are as follows:
    
      Perf Sched Pie     UnixBench     DMA Test      SoC model
                                     (iops/speed)
           +1.7%           +3.4%       +2.4/3.5%     JZ4775
           +4.6%           +5.2%       +2.7/3.7%     JZ4780 (SMP off)
           +6.3%          +13.4%       +4.3/5.1%     JZ4780 (SMP on)
           +2.6%           +4.2%       +3.1/3.9%     X1000
           +3.8%           +7.8%       +2.9/6.1%     X1830
           +1.9%           +1.7%       +2.1/3.2%     X2000 (SMT off)
           +6.7%           +2.5%       +2.3/2.8%     X2000 (SMT on)
    
    This patch also fixes the problem of incorrect detection of JZ4780 L2
    cache parameters, and save about 9kiB (ARCH=mips make ci20_defconfig).
    
    Tested-by: H. Nikolaus Schaller <hns@goldelico.com>
    Signed-off-by: 周琰杰 (Zhou Yanjie) <zhouyanjie@wanyeetech.com>

--- arch/mips/mm/cache.c
+++ arch/mips/mm/cache.c
@@ -190,6 +190,7 @@ void cpu_cache_init(void)
 
 		r3k_cache_init();
 	}
+
 	if (cpu_has_4k_cache) {
 		extern void __weak r4k_cache_init(void);
 
@@ -200,6 +201,12 @@ void cpu_cache_init(void)
 		extern void __weak octeon_cache_init(void);
 
 		octeon_cache_init();
+	}
+
+	if (cpu_has_ingenic_cache) {
+		extern void __weak ingenic_cache_init(void);
+
+		ingenic_cache_init();
 	}
 
 	setup_protection_map();
